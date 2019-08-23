80c86604a0eb9b18636c41e0aa9fdd9523497313
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-08-23 16:22:34 -0400

    Updated components

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import "./Card.css";
 
 export default class Card extends Component {
   render() {
-    return <div>Card</div>;
+    const { img, suit, val, deg, margin } = this.props;
+    return (
+      <img
+        className="Card"
+        src={img}
+        alt={`${val} of ${suit}`}
+        style={{ transform: `rotate(${deg}deg)`, marginTop: `${margin}px` }}
+      />
+    );
   }
 }
--- src/Deck.js
+++ src/Deck.js
@@ -1,21 +1,79 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Card from "./Card";
+import "./Deck.css";
 
 export default class Deck extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      deckID: "",
+      remaining: "",
+      cards: [],
+    };
     this.handleClick = this.handleClick.bind(this);
+    this.generateRandom = this.generateRandom.bind(this);
   }
-  handleClick() {
-    console.log("new card");
+
+  async componentDidMount() {
+    let response = await axios.get(
+      "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+    );
+    console.log(response);
+    this.setState({
+      deckID: response.data.deck_id,
+      remaining: response.data.remaining,
+    });
   }
+
+  async handleClick() {
+    let newCard = await axios.get(
+      `https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/`
+    );
+    const { image, suit, value, code } = newCard.data.cards[0];
+    const cardInfo = {
+      image,
+      suit,
+      value,
+      code,
+      randomDegree: this.generateRandom(45),
+      randomMargin: this.generateRandom(20),
+    };
+    this.setState(st => ({
+      remaining: st.remaining - 1,
+      cards: [...st.cards, cardInfo],
+    }));
+  }
+
+  generateRandom(num) {
+    return Math.floor(Math.random() * num) + Math.floor(Math.random() * -num);
+  }
+
   render() {
     return (
-      <div>
-        <button onClick={this.handleClick}>New Card</button>
-        <Card />
-        <Card />
+      <div className="Deck">
+        {this.state.remaining > 0 ? (
+          <button className="Deck-button" onClick={this.handleClick}>
+            Draw New Card
+          </button>
+        ) : (
+          <p>That's the end of the deck!</p>
+        )}
+        <div className="Deck-cards">
+          {this.state.cards.map(card => (
+            <Card
+              key={card.code}
+              img={card.image}
+              suit={card.suit}
+              val={card.value}
+              deg={card.randomDegree}
+              margin={card.randomMargin}
+            />
+          ))}
+        </div>
+        <p className="Deck-remaining">
+          Cards Remaining: {this.state.remaining}
+        </p>
       </div>
     );
   }
